de6e4e0b2ff2f367855b30fc713909fd557cb537
Toshihiro Shimizu <shimizu.toshihiro@gmail.com>
2014-11-02 14:21:31 +0900

    FIX ガードのインデント

--- Fibo.hs
+++ Fibo.hs
@@ -3,9 +3,9 @@ fib 1 = 1
 fib n = fib (n - 1) + fib (n - 2)
 
 fib2 n
-   | n == 0    = 0
-   | n == 1    = 1
-   | otherwise = fib2 (n - 1) + fib2 (n - 2)
+    | n == 0    = 0
+    | n == 1    = 1
+    | otherwise = fib2 (n - 1) + fib2 (n - 2)
 
 fib3 n = case n of
   0 -> 0
