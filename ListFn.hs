f5be3d414620619c0af8849a732a66b2d1f7d6cc
Toshihiro Shimizu <shimizu.toshihiro@gmail.com>
2014-11-02 15:09:12 +0900

    Add List関係の関数の実装

---
+++ ListFn.hs
@@ -0,0 +1,32 @@
+module ListFn where
+
+length' []     = 0
+length' (_:xs) = 1 + length' xs
+
+take' 0 x      = []
+take' n []     = []
+take' n (x:xs) = x:(take' (n - 1) xs)
+
+drop' 0 x      = x
+drop' n []     = []
+drop' n (x:xs) = drop' (n - 1) xs
+
+reverse' []     = []
+reverse' (x:xs) = (reverse' xs) ++ [x]
+
+sum' []      = 0
+sum' (x:xs)  = x + sum' xs
+
+product' []     = 1
+product' (x:xs) = x * product' xs
+
+fact' n = product' [1..n]
+
+main = do
+  print $ length' "abcdef"
+  print $ take' 3 "abcdef"
+  print $ drop' 3 "abcdef"
+  print $ reverse' "abcdef"
+  print $ sum' [1, 2, 3, 4, 5]
+  print $ product' [1, 2, 3, 4, 5]
+  print $ fact' 5
