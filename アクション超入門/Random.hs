3e2229ac8f6961b5a8fddc0e055895fc25e0d9be
Toshihiro Shimizu <shimizu.toshihiro@gmail.com>
2014-11-02 19:53:27 +0900

    Applicativeを使った例

--- アクション超入門/Random.hs
+++ アクション超入門/Random.hs
@@ -1,10 +1,15 @@
 import System.Random
+import Control.Applicative
 
 randAlpha :: IO Char
 randAlpha = getStdRandom $ randomR ('A', 'Z')
 
 dice :: IO Int
 dice = getStdRandom $ randomR (1, 6)
+
+add x y = return $ x + y
+
+inc x = x + 1
 
 main = do
   r <- randAlpha
@@ -27,4 +32,6 @@ main = do
   print r
   r <- dice
   print r
-  
+
+  print =<< (add =<< return 1) =<< return 2
+  print =<< inc <$> return 1
