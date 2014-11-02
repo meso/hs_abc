ad2f9f2700d67f11c470bbda1675a5da22874cef
Toshihiro Shimizu <shimizu.toshihiro@gmail.com>
2014-11-02 16:04:12 +0900

    Add タプル使った演習

---
+++ TupleExam.hs
@@ -0,0 +1,12 @@
+-- ax + by = c
+-- bx - ay = d
+-- bp - aq = d
+
+perpPoint (p, q) (a, b, c) = (x, y)
+  where
+    x = (a * c + b * d) / (a * a + b * b)
+    y = (b * c - a * d) / (a * a + b * b)
+    d = b * p - a * q
+
+main = do
+  print $ perpPoint (0, 2) (1, -1, 0)
